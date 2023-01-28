28d387513906d75a5c006527719abd6eddbd9913
Kamalesh-Dev-Web <kamalsurea619@gmail.com>
2023-01-28 11:42:17 +0530

    Abstract class

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("hELLO WORLD");
+// Basic class
+var Car = /** @class */ (function () {
+    function Car(model, color, isElectric) {
+        this.model = model;
+        this.color = color;
+        this.isElectric = isElectric;
+    }
+    Car.prototype.drive = function () {
+        var engineIsStarted = this.startEngine();
+    };
+    Car.prototype.startEngine = function () {
+        return true;
+    };
+    return Car;
+}());
+// ABSTRACT
+var Character = /** @class */ (function () {
+    function Character(name, health, speed) {
+        this.name = name;
+        this.speed = speed;
+        this.health = health;
+    }
+    return Character;
+}());
+var Goblin = /** @class */ (function (_super) {
+    __extends(Goblin, _super);
+    function Goblin(name, health, speed) {
+        return _super.call(this, name, health, speed) || this;
+    }
+    Goblin.prototype.calculatePower = function () {
+        return this.health * this.speed;
+    };
+    Goblin.prototype.characterName = function () {
+        return this.name;
+    };
+    return Goblin;
+}(Character));
+var Wizard = /** @class */ (function (_super) {
+    __extends(Wizard, _super);
+    function Wizard(name, health, speed) {
+        return _super.call(this, name, health, speed) || this;
+    }
+    return Wizard;
+}(Goblin));
+var Wizzy = new Wizard("wizzy", 100, 25);
+console.log(Wizzy.calculatePower());
--- main.ts
+++ main.ts
@@ -1,1 +1,64 @@
-console.log("hELLO WORLD")
+console.log("hELLO WORLD");
+
+// Basic class
+
+class Car {
+  public model: string;
+  public color: string;
+  public isElectric: boolean;
+
+  constructor(model: string, color: string, isElectric: boolean) {
+    this.model = model;
+    this.color = color;
+    this.isElectric = isElectric;
+  }
+
+  public drive(): void {
+    const engineIsStarted: boolean = this.startEngine();
+  }
+
+  private startEngine(): boolean {
+    return true;
+  }
+}
+
+// ABSTRACT
+abstract class Character {
+  public name: string;
+  public health: number;
+  public speed: number;
+
+  constructor(name: string, health: number, speed: number) {
+    this.name = name;
+    this.speed = speed;
+    this.health = health;
+  }
+
+  public abstract calculatePower(): number;
+
+  public abstract characterName(): string;
+}
+
+class Goblin extends Character {
+  constructor(name: string, health: number, speed: number) {
+    super(name, health, speed);
+  }
+
+  public calculatePower(): number {
+    return this.health * this.speed;
+  }
+
+  public characterName(): string {
+    return this.name;
+  }
+}
+
+class Wizard extends Goblin {
+  constructor(name: string, health: number, speed: number) {
+    super(name, health, speed);
+  }
+}
+
+const Wizzy = new Wizard("wizzy", 100, 25);
+
+console.log(Wizzy.calculatePower());
